fd13dd91d0caec81f425767dd4a4e7df7f9cf14c
Sarinj <sarin19222@gmail.com>
2024-09-23 22:49:25 +0700

    adjust UI

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,16 +1,9 @@
 import SigninForm from '@/components/form/SigninForm'
 import SignupForm from '@/components/form/SignupForm'
-import { useAuth } from '@/hooks/useAuthentication'
 import { useState } from 'react'
-import { Navigate } from 'react-router-dom'
 
 export default function LoginPage() {
   const [isRegister, setIsRegister] = useState(false)
-  const { isAuthenticated } = useAuth()
-
-  if (isAuthenticated) {
-    return <Navigate to='/' />
-  }
 
   return (
     <main className='flex size-full h-screen items-center justify-center'>
